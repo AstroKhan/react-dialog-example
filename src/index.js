96dec43fdc2d5671fa575cb387a868f9e3a334c0
AstroGit <masaladosa6@gmail.com>
2022-07-15 18:06:09 +0300

    add date

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import WebFontLoader from "webfontloader";
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
 
 import App from "./App";
 import "./css/index.css";
@@ -10,6 +12,8 @@ WebFontLoader.load({
     families: ["Open Sans:300,400,700"],
   },
 });
+
+dayjs.locale("ru");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
